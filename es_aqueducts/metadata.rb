4bbed684a65480e15239b3a2da69bc77a0e28b73
Qiangji <qiangji1991@gmail.com>
2014-08-21 11:15:49 +0800

    Update metadata.rb

--- es_aqueducts/metadata.rb
+++ es_aqueducts/metadata.rb
@@ -7,6 +7,18 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.2'
 
 
+recipe "es_aqueducts::default", "install es on node"
+
+recipe "es_aqueducts::start_es", "start es service only if es exists on node"
+
+recipe "es_aqueducts::kill_es", "kill es process"
+
+recipe "es_aqueducts::reload_es", "restart es"
+
+recipe "es_aqueducts::update_es_conf" ,"shutdown es , updata es conf, start es"
+
+recipe "es_aqueducts::remove_es", " remove es from node"
+
 #depends 'java'
 
 supports 'centos'
