9df44e7609d46f08f7e4b33fff0bc38224f7dd2a
Qiangji <qiangji1991@gmail.com>
2014-08-21 11:16:27 +0800

    Update metadata.rb

--- nginx_aqueducts/metadata.rb
+++ nginx_aqueducts/metadata.rb
@@ -6,6 +6,17 @@ description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.4'
 
+recipe "nginx_aqueducts::default", "install nginx on node"
+
+recipe "nginx_aqueducts::shutdown_nginx", "shutdown nginx in soft manner"
+
+recipe "nginx_aqueducts::kill_nginx", "kill nginx process"
+
+recipe "nginx_aqueducts::start_nginx", "start nginx"
+
+recipe "nginx_aqueducts::reload_nginx", "restart nginx"
+
+recipe "nginx_aqueducts::updata_nginx_conf", "update nginx conf"
 
  
 supports 'centos'
