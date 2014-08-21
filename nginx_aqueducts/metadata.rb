bfb6bdf41fad51f5034f41ffa47725c575f3e3cf
Qiangji <qiangji1991@gmail.com>
2014-08-21 11:15:04 +0800

    Update metadata.rb

--- nginx_aqueducts/metadata.rb
+++ nginx_aqueducts/metadata.rb
@@ -6,4 +6,17 @@ description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.4'
 
+
+recipe "es_aqueducts::default", "install es on node"
+
+recipe "es_aqueducts::start_es", "start es service only if es exists on node"
+
+recipe "es_aqueducts::kill_es", "kill es process"
+
+recipe "es_aqueducts::reload_es", "restart es"
+
+recipe "es_aqueducts::update_es_conf" ,"shutdown es , updata es conf, start es"
+
+recipe "es_aqueducts::remove_es", " remove es from node"
+ 
 supports 'centos'
